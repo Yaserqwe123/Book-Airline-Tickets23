3117c74e3adb3989c65414fa6c9eba8af85d5a84
Yaserqwe123 <134320318+Yaserqwe123@users.noreply.github.com>
2023-05-22 22:09:07 +0300

    Add files via upload

---
+++ images/Student.java
@@ -0,0 +1,25 @@
+
+package lecture1;
+
+public class Student {
+    //class Attributes 
+    String name;
+    String id;
+    float gpa;
+    String college;
+    String department;
+    int age;
+
+    public void setName(String name) {
+       
+            this.name = name;
+        
+       
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+    
+}
